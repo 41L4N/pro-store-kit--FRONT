7ab42507c4e281eebfd96c385185f9944e6d4c6e
vengavic64 <vengavic64@gmail.com>
2026-06-04 10:25:54 -0400

    fix: remover imports de estilos de PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,6 @@ import PrimeVue from 'primevue/config'
 import App from './App.vue'
 import router from './router'
 
-// Importar estilos de PrimeVue
-import 'primevue/resources/themes/lara-light-blue/theme.css'
-import 'primevue/resources/primevue.css'
-import 'primeicons/primeicons.css'
-import 'primeflex/primeflex.css'
-
 const app = createApp(App)
 
 app.use(createPinia())
