01cfdb9a116fee81562a74f9a6803f8df89325bc
vengavic64 <vengavic64@gmail.com>
2026-06-04 10:20:51 -0400

    fix: configurar PrimeVue en main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
+
+// Importar estilos de PrimeVue
+import 'primevue/resources/themes/lara-light-blue/theme.css'
+import 'primevue/resources/primevue.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
